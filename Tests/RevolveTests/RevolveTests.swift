525ba14d3030952f5d305fedb59dbd1f9e89e905
Oti Oritsejafor <otioritsejafor@gmail.com>
2023-04-26 07:07:38 -0400

    Initial Commit

---
+++ Tests/RevolveTests/RevolveTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Revolve
+
+final class RevolveTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Revolve().text, "Hello, World!")
+    }
+}
